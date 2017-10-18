69c2ae9330f281d7a6e76e60629468379bd89998
U-DESKTOP-AUMAJUM\Nick <blottn@tcd.ie>
2017-10-18 10:25:26 +0100

    added project

---
+++ src/main/java/nblott/org/replay/Example.java
@@ -0,0 +1,12 @@
+package nblott.org.replay;
+
+/**
+ * Created by Nick on 18/10/2017.
+ */
+
+public class Example {
+
+    public static void doThing() {
+        System.out.println("Hi");
+    }
+}
