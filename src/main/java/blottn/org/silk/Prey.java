a96609a221e914ab0ee1afcef3c57b7b25fd9207
U-DESKTOP-AUMAJUM\Nick <blottn@tcd.ie>
2017-10-19 16:39:46 +0100

    added prey class to describe a web event

---
+++ src/main/java/blottn/org/silk/Prey.java
@@ -0,0 +1,18 @@
+package blottn.org.silk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+/**
+ * Created by Nick on 19/10/2017.
+ * Describes what type of event was caught.
+ * Largely a struct to organise data
+ */
+
+public class Prey {
+
+    private long instant;
+    private MotionEvent event;
+    private AppCompatActivity clazz;
+
+}
