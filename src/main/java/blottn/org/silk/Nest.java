1e49214a05f9813e5a0c88812e81c637b6e63478
U-DESKTOP-AUMAJUM\Nick <blottn@tcd.ie>
2017-10-20 14:31:57 +0100

    created database for storing events, this database is primitive as most of the analysis should be done in the rendering app. This is to preserve as much information as possible

--- src/main/java/blottn/org/silk/Nest.java
+++ src/main/java/blottn/org/silk/Nest.java
@@ -1,6 +1,8 @@
 package blottn.org.silk;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
@@ -16,10 +18,24 @@ public class Nest extends SQLiteOpenHelper {
 
 
     public static class WebDatabaseEntry implements BaseColumns {
-        public static final String TABLE_NAME = "entry";
-        public static final String COLUMN_NAME_TITLE = "title";
-        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+        public static final String TABLE_NAME = "events";
+
+        public static final String COLUMN_NAME_INSTANT = "instant";     //placeholder basic information
+        public static final String COLUMN_NAME_TYPE = "type";
     }
+
+    // SQL command to create the table
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + WebDatabaseEntry.TABLE_NAME + " (" +
+                    WebDatabaseEntry._ID + " INTEGER PRIMARY KEY," +
+                    WebDatabaseEntry.COLUMN_NAME_INSTANT + " BIGINT," +
+                    WebDatabaseEntry.COLUMN_NAME_TYPE + " TEXT)";
+
+    // SQL command to delete the table
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + WebDatabaseEntry.TABLE_NAME;
+
+
 
     public Nest(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,11 +43,56 @@ public class Nest extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
+        onCreate(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        onUpgrade(db, oldVersion, newVersion);
     }
+
+    public void insert(Prey prey) {
+        // Gets the data repository in write mode
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(WebDatabaseEntry.COLUMN_NAME_INSTANT,prey.getInstant());
+        values.put(WebDatabaseEntry.COLUMN_NAME_TYPE,"wowow");
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(WebDatabaseEntry.TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public Cursor query(long time, String type) {
+        // Gets the data repository in write mode
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] projection = {
+                WebDatabaseEntry._ID,
+                WebDatabaseEntry.COLUMN_NAME_INSTANT,
+                WebDatabaseEntry.COLUMN_NAME_TYPE
+        };
+
+        String selection = WebDatabaseEntry.COLUMN_NAME_TYPE + " = ?";
+        String[] selectionArgs = {
+                type
+        };
+
+        String sortOrder = WebDatabaseEntry.COLUMN_NAME_INSTANT + " ASC";
+
+        Cursor cursor = db.query(
+                WebDatabaseEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+        return cursor;
+    }
+
 }
