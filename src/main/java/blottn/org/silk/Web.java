41e561b9053f87765a87c28ec08042722dd6edb0
U-DESKTOP-AUMAJUM\Nick <blottn@tcd.ie>
2017-10-20 09:32:00 +0100

    added PPrey creation and passing to the Spiders

--- src/main/java/blottn/org/silk/Web.java
+++ src/main/java/blottn/org/silk/Web.java
@@ -2,8 +2,12 @@ package blottn.org.silk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,16 +30,10 @@ public class Web {
     
     public static void touch(AppCompatActivity activity, MotionEvent event) {
         //on touch, check if the activity has touched a web already.
-        if (tethers.containsKey(activity)) {
-            //seen
-
+        if (!tethers.containsKey(activity)) {
+            tether(activity, new Spider()); //if it's not yet seen, add it
         }
-        else {
-            //unseen
-            //tether it, add a spider
-            tether(activity, new Spider());
-            tethers.get(activity).snare(activity, event);
-        }
+        tethers.get(activity).snare(activity, event);
     }
 
     /******************************************************************************
@@ -57,5 +55,37 @@ public class Web {
     public void snare(AppCompatActivity activity, MotionEvent event) {
         //handle data
         System.out.println("Motion Detected!");
+        // check views involved
+        List<Field> fields = findAllWebbedViews(activity);
+        if (fields == null) {
+//            fields = findAllViews();
+        }
+        Prey prey = new Prey(new Date().getTime(),event,activity, null);    //TODO find the views involved
+        for (Spider spider : spiders) {
+            spider.feed(prey);
+        }
+    }
+
+    private List<Field> findAllWebbedViews(AppCompatActivity activity) {
+        List<Field> found = new ArrayList<>();
+        for (Field field : activity.getClass().getDeclaredFields()) {
+            boolean wasAccessible = field.isAccessible();
+            field.setAccessible(true);
+            try {
+                if (isWebbedView(field) && field.get(activity) != null) {
+                    found.add(field);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();    //This shouldn't happen anyways as we have set the field as accessible
+            }
+            field.setAccessible(wasAccessible);
+        }
+        return found;
+    }
+
+    private boolean isWebbedView(Field field) {
+        return field.isAnnotationPresent(Webbed.class)
+                && (field.getAnnotation(Webbed.class)).enabled()
+                && (View.class.isAssignableFrom(field.getType()));
     }
 }
